d64bbd99458e48c1eb7ad2d76d32fa8bb3ac65e0
Knovour Zheng <knovour@gmail.com>
2016-03-15 02:24:50 +0800

    Site updated: 2016-03-15 02:24:49

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
   if(location.protocol !== 'https:' && !location.host.includes('0.0.0.0'))
-    location.protocol = 'https:';
+    location.protocol = 'https';
 
   // Caption
   $('.article-entry').each(function(i){
